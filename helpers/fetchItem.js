0f51c51850220fb8d5b29884056727a6f61e9e74
saint-lag <maiasena.gabriel@gmail.com>
2022-02-02 17:06:50 -0300

    [Gabriel Sena] Projeto Shopping Cart

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,23 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetch = require('node-fetch');
+
+const fetchItem = async () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.results);
+    return data.results;
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
 };
+
+// const results = async () => {
+  // const require = await fetchItem();
+  // console.log(require);
+// };
+
+// results();
 
 if (typeof module !== 'undefined') {
   module.exports = {
